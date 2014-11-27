565c95e410fa24a07d7658741bde8628c18a597d
srikanthkarangolla <srikanthraokarangolla@gmail.com>
2014-11-27 22:11:16 +0530

    add one to many association

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Onetomanyass::Application.config.secret_token = '4dd274131c1c23dbd0f52ee60cc9a1045dfba2edfe89db5e0609397100e050a2243d69a32776797aab4efbd948aba097b93260a382b5636eb257389bbf0f83f3'
---
+++ db/migrate/20141127093751_add_school_id_to_student.rb
@@ -0,0 +1,6 @@
+class AddSchoolIdToStudent < ActiveRecord::Migration
+  def change
+    add_column :students, :school_id, :integer
+
+  end
+end
